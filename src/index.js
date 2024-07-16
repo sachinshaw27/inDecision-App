e66890b84ce4927a385372125b4f7407e993037d
sachinshaw27 <sachinshaw623@gmail.com>
2024-07-16 20:08:34 +0530

    change file name

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import app from './app';
+import App from './ App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>
 );
 
